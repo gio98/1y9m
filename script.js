b29e6b67059744a7ba14629d44a9ebd6497830d8
gio98 <gio982cap@live.it>
2023-07-22 15:17:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,10 +9,22 @@ checkbox.addEventListener("change", function() {
     var letter = document.getElementById("letter");
     // Si cambia la proprietà top dello stile dell'elemento
     letter.style.top = "-41px";
+    var front = document.getElementById("front");
+    // Si cambia la proprietà top dello stile dell'elemento
+    front.style.backgroundImage = "none";
+    var back = document.getElementById("back");
+    // Si cambia la proprietà top dello stile dell'elemento
+    back.style.backgroundImage = "none";
   } else {
          // Si seleziona l'elemento con id="letter"
          var letter = document.getElementById("letter");
          // Si cambia la proprietà top dello stile dell'elemento
          letter.style.top = "76px";
+         var front = document.getElementById("front");
+          // Si cambia la proprietà top dello stile dell'elemento
+          front.style.backgroundImage = "url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/144467/after.png')";
+          var back = document.getElementById("back");
+          // Si cambia la proprietà top dello stile dell'elemento
+          back.style.backgroundImage = "url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/144467/before1.png')";
   }
 });
